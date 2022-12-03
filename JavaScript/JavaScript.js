fcd0434ef95a43a58148478489522e57cc06b408
R_A_M_A_Z_O_N <orashirizvon@gmail.com>
2022-12-03 15:00:48 +0300

    first commit

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,14 @@
+
+  var x = prompt("x", "");
+  var n = prompt("n", "");
+
+  if (n < 0) {
+    alert(`Stepen` + n +
+      `Ne poderchiwat`);
+  } else {
+    p=x*n;
+    document.write(p);
+  
+  }
+
+
